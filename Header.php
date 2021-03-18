384305637c45d092e24c2a3d7ba2222dda4a1a2c
Shidorien <gaetangerard@hotmail.fr>
2021-03-18 14:40:53 +0100

    Nouveaux header

--- Header.php
+++ Header.php
@@ -1,35 +1,3 @@
-
-<header>
-<!--<?php //session_start ();?>
-    <ul class="container">
-    <li class="Fl"><a href="PageAccueil.php"><img src="/Cinevent/images/logo.png"></a></li>
-        <?php if (isset($_SESSION['email']))
-        {?>
-
-            
-            <li><a class="Boutton_Menu" href="script/deconnexion.php">Deconnexion</a></li>
-            <li><a class="Boutton_Menu" href="PagePanier.php">Panier</a></li>
-        
-        <?php
-        }
-        else
-        {
-            ?>
-
-            
-                  <li><a class="Boutton_Menu" href="PageConnexion.php">Connexion</a></li>
-        <?php
-    }
-    ?>
-
-        <li><a class="Boutton_Menu" href="PageContact.php">Contact</a></li>
-        <li><a class="Boutton_Menu" href="PageAPropos.php">a propos</a></li>
-
-        <li><input type="search" id="site-search" name="q" aria-label="Search through site content"><a class="Boutton_Menu">Search</a></li>
-    </ul>
-</header>-->
-
-
 <header>
 <?php session_start ();?>
 <div class="Head">
@@ -38,10 +6,24 @@
     <div class="HorsLogo">
         <div class="Bouton">
         <ul>
-            <li class="Boutton_Menu"><a href="PageContact.php">Contact</a></li>
+
+        <?php if (isset($_SESSION['email']))
+        {?>
+            <li class="Boutton_Menu"><a href="script/deconnexion.php">Deconnexion</a></li>
+        <?php
+        }
+        else
+        {
+        ?>
+            <li class="Boutton_Menu"><a href="PageConnexion.php">Connexion</a></li>
+        <?php
+        }
+        ?>
+
+          <li class="Boutton_Menu"><a href="PageContact.php">Contact</a></li>
             <li class="Boutton_Menu"><a href="PageAPropos.php">a propos</a></li>
-            <!--<li><img src="/GerardRadeCinevent/images/loupe.png"></li>-->
-            <li><input type="search"></li>
+            <li class="Boutton_Recherche"><img src="/GerardRadeCinevent/images/loupe.png"></li>
+            <li class="Barre_Recherche"><input type="search" ></li>
         </ul>
     </div>
 
