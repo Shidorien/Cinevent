d20804f1200f63d77c0509b30d14dfd228e31065
Shidorien <gaetangerard@hotmail.fr>
2021-06-01 11:05:59 +0200

    df

--- Vues/Connexion.php
+++ Vues/Connexion.php
@@ -1,17 +1,18 @@
-<div class="box"> <!-- carré blanc -->
-            <form action = "fct.php"  method="POST" name="Connexion1">
-                <div>
-                    <label for="mail">e-mail :</label>
-                    <input type="email" id="email" name="email" class="input">
-                </div>
-
-                <div>
-                    <label for="password">mot de passe :</label>
-                    <Input type="password" id="motDePasse" name="motDePasse" class="input"> <br>
-                </div>
-                <ul>
-                    <input type="submit" name="connexion" value="Connexion" class="button" />
-                    <li><a href="index.php?srv=5">Inscription</a></li>
-                </ul>
-            </form>
+<div class="Connexion">
+    <form action="fct.php" method="POST" name="Connexion1">
+        <h2><?php $Connexion ?></h2>
+        <div>
+            <label for="mail">e-mail</label><br>
+            <input type="email" id="email" name="email" class="Saisie">
         </div>
+
+        <div>
+            <label for="password">mot de passe</label><br>
+            <Input type="password" id="motDePasse" name="motDePasse" class="Saisie"> <br>
+        </div>
+        <div>
+            <input type="submit" name="connexion" value="Connexion" class="Bouton_Menu" /><br>
+            <a href="index.php?srv=5">Inscription</a>
+        </div>
+    </form>
+</div>
